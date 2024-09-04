363e0fb6699a3402ad2753e69f27685b73702ec6
avgra3 <antonygradillas@gmail.com>
2024-09-03 22:39:36 -0700

    removed visibility of collision circle

--- asteroid.py
+++ asteroid.py
@@ -1,17 +1,20 @@
 from circleshape import CircleShape
-from pygame import draw
+from pygame import draw, image, transform
 from constants import ASTEROID_MIN_RADIUS
 import random
-
+from pathlib import Path
 
 class Asteroid(CircleShape):
     def __init__(self, x, y, radius):
         super().__init__(x, y, radius)
+        self.image = transform.scale(image.load(Path("assets/asteroid.png")), (self.radius * 2.1, self.radius * 2.1))
 
     def draw(self, screen):
-        width = 2
-        draw.circle(screen, color="white",
+        width = -1 # Was: 2
+        circle = draw.circle(screen, color="white",
                     center=self.position, radius=self.radius, width=width)
+        screen.blit(self.image, circle)
+
 
     def update(self, dt):
         self.position += (self.velocity * dt)
--- player.py
+++ player.py
@@ -32,7 +32,7 @@ class Player(CircleShape):
         return [a, b, c]
 
     def draw(self, screen):
-        pygame.draw.polygon(screen, "white", self.triangle(), 2)
+        pygame.draw.polygon(screen, "purple", self.triangle(), 2)
 
     def rotate(self, dt):
         self.rotation += (PLAYER_TURN_SPEED * dt)
